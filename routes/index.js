578ca9dbed41c2d18e71051be3dd880452a9f9a2
5nahalf <5nahalf@comcast.net>
2015-05-12 16:20:12 -0500

    it works111

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var mongoose = require('mongoose');
+var assignModel = require('../models/assignment');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.resolve(__dirname, '../views/index.html'));
 });
+
+router.get('/assignment/:id', function(req, res, next){
+  assignModel.find(function(err, assignment){
+    if(err) return next(err);
+    res.json(assignment);
+  });
+});
+//this posts to the file done and tested aok
+router.post('/', function(req, res, next){
+  assignModel.create(req.body, function(err, post){
+    if(err) return next(err);
+    res.json(assignModel);
+  });
+});
+//this is for the update code done not tested
+router.put('/assignment/:id', function(req, res, next) {
+  assignModel.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+//this will delete
+router.delete('/assignment/:id', function(req, res, next) {
+  assignModel.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) response.json(err);
+    res.json(post);
+  });
+});
+
+
+/* GET home page. */
+
 
 module.exports = router;
