d844f229f46bd3ef0ce25f52f54c8c08bda5fc46
5nahalf <5nahalf@comcast.net>
2015-05-12 16:36:28 -0500

    add assignment.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var assignment = require('./routes/assignment');
 var app = express();
 
 var mongoose = require('mongoose');
@@ -39,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/assignment', assignment);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/assignment.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var mongoose = require('mongoose');
+var assignModel = require('../models/assignment');
+
+
+router.get('/', function(req, res, next){
+    assignModel.find(function(err, assignment){
+        if(err) return next(err);
+        res.json(assignment);
+    });
+});
+//this posts to the file done and tested aok
+router.post('/', function(req, res, next){
+    assignModel.create(req.body, function(err, post){
+        if(err) return next(err);
+        res.json(assignModel);
+    });
+});
+//this is for the update code done not tested
+router.put('/:id', function(req, res, next) {
+    assignModel.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+//this will delete
+router.delete('/:id', function(req, res, next) {
+    assignModel.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+        if (err) response.json(err);
+        res.json(post);
+    });
+});
+
+
+/* GET home page. */
+
+
+module.exports = router;
